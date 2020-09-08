d6913c47bf180c070e75a5f26d1d62fe0e9e3463
mimamulhassankhan <pappuhassan440@gmail.com>
2020-09-08 15:23:48 +0600

    Updates

---
+++ precache-manifest.6756f2f5b5964199fc5d73fbd6c3fa81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a421f760d7026fee2294a22dd1b736db",
+    "url": "/online-course-react/index.html"
+  },
+  {
+    "revision": "8dd66a3d7639d295ccff",
+    "url": "/online-course-react/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8360396cb24e71a174b7",
+    "url": "/online-course-react/static/css/main.4ddfbea1.chunk.css"
+  },
+  {
+    "revision": "8dd66a3d7639d295ccff",
+    "url": "/online-course-react/static/js/2.5f155fb3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/online-course-react/static/js/2.5f155fb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8360396cb24e71a174b7",
+    "url": "/online-course-react/static/js/main.161f41a6.chunk.js"
+  },
+  {
+    "revision": "70d3b4ea08a79783641a",
+    "url": "/online-course-react/static/js/runtime-main.d22a63cf.js"
+  }
+]);
